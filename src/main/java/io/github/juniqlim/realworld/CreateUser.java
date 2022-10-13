14db2fe257e15e9601f520dddf744f666e0acfbf
juniq <juniqlim@gmail.com>
2022-10-13 11:25:07 +0900

    feat(User): implement CreateUser

--- src/main/java/io/github/juniqlim/realworld/CreateUser.java
+++ src/main/java/io/github/juniqlim/realworld/CreateUser.java
@@ -1,8 +1,18 @@
 package io.github.juniqlim.realworld;
 
 public class CreateUser {
+    private final UserRepository userRepository;
+    private final Token token;
+
+    public CreateUser(UserRepository userRepository, Token token) {
+        this.userRepository = userRepository;
+        this.token = token;
+    }
+
     public User user(Request request) {
-        return null;
+        User user = request.user(token);
+        userRepository.save(user);
+        return user;
     }
 
     static class Request {
@@ -14,6 +24,10 @@ public class CreateUser {
             this.username = username;
             this.email = email;
             this.password = password;
+        }
+
+        public User user(Token token) {
+            return new User(token.token(), password, username, email);
         }
     }
 }
--- src/test/java/io/github/juniqlim/realworld/CreateUserTest.java
+++ src/test/java/io/github/juniqlim/realworld/CreateUserTest.java
@@ -6,8 +6,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CreateUserTest {
     @Test
-    void test() {
-        User user = new CreateUser().user(new CreateUser.Request("Jacob", "jake@jake.jake", "jakejake"));
-        assertThat(user).isEqualTo(new User(Token.FAKE.token(), "123", "Jacob", "jake@jake.jake", "I work at statefarm", null));
+    void saveAndFind() {
+        UserRepository userRepository = new UserRepository();
+        String token = Token.FAKE.token();
+
+        User user = new CreateUser(userRepository, Token.FAKE).user(new CreateUser.Request("Jacob", "jake@jake.jake", "jakejake"));
+        User findedUser = userRepository.findByToken(token);
+
+        assertThat(user).isEqualTo(findedUser);
     }
 }
